1cccac66a68fed500438d66305d9854c0b55197a
Adal <113269863+Adal4@users.noreply.github.com>
2025-12-03 17:29:31 +0100

    Uodate interface number

--- scripts/install.py
+++ scripts/install.py
@@ -4,12 +4,12 @@ from pathlib import Path
 addonversion = "0.3.15"
 DBMVoiceVersion = "18"
 
-Interface = "110200"
-InterfaceMists = "50501"
+Interface = "110207"
+InterfaceMists = "50502"
 InterfaceCata = "40402"
-InterfaceWrath = "30405"
+InterfaceWrath = "38000"
 InterfaceTBC = "20504"
-InterfaceClassic = "11507"
+InterfaceClassic = "11508"
 
 DIPPKG_PATH = Path("G:\Dev\DBM-VoicePack\zip-files")
 ADDON_path = Path("G:\Dev\DBM-VoicePack")
